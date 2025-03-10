9c0f22a57c618c6280e2bcfbaa2d639aaf315811
Max McWhae <20085898@tafe.wa.edu.au>
2025-03-10 15:34:07 +0800

    Added stubs to PlayerHashMap class

---
+++ app/player_hash_map.py
@@ -0,0 +1,15 @@
+class PlayerHashMap:
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __setitem__(self, key, name):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __delitem__(self, key):
+        pass
