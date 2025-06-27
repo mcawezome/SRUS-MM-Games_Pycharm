3b7fc16c1759146aac8568a36df5ef940efe2b3f
Max McWhae <20085898@tafe.wa.edu.au>
2025-06-28 05:29:57 +0800

    Step 6

--- app/player_bst.py
+++ app/player_bst.py
@@ -9,3 +9,32 @@ class PlayBST:
     @root.setter
     def root(self, value):
         self._root = value
+
+    def insert(self, player):
+        if self.root is None:
+            self.root = PlayerBNode(player)
+        else:
+            self._insert(player, self.root)
+
+    def _insert(self, player, node):
+        if player.name < node.player.name:
+            if node.left is None:
+                node.left = PlayerBNode(player)
+            else:
+                self._insert(player, node.left)
+        else:
+            if node.right is None:
+                node.right = PlayerBNode(player)
+            else:
+                self._insert(player, node.right)
+
+    def search(self, name):
+        if self.root is None:
+            return None
+        else:
+            return self._search(name, self.root)
+
+    def _search(self, name, node):
+        if node is None:
+            return None
+        elif node.player.name == name:
